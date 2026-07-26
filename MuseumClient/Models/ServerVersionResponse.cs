22777fc36a02fe433f0a9a1ba37179fc3c8a9647
Герман Исаев <germanisaev422@gmail.com>
2026-07-26 14:22:55 +0300

    dto принятия клентом от сервера версии

---
+++ MuseumClient/Models/ServerVersionResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MuseumClient.Models
+{
+    public class ServerVersionResponse
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; } = string.Empty;
+
+        [JsonPropertyName("data")]
+        public ServerVersionDto? Data { get; set; }
+    }
+
+    public class ServerVersionDto
+    {
+        [JsonPropertyName("version")]
+        public string Version { get; set; } = string.Empty;
+    }
+}

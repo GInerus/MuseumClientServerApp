cceadf62ed438c9d69f8de1eb00da5fbb12b8cfc
Герман Исаев <germanisaev422@gmail.com>
2026-07-26 14:19:03 +0300

    Контроллер для сервера. Теперь api выдаёт версию сервера

---
+++ MuseumServer/Controllers/SystemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace MuseumServer.Controllers
+{
+    [ApiController]
+    [Route("api/system")]
+    public class SystemController : ControllerBase
+    {
+        // GET: api/system/version
+        [HttpGet("version")]
+        public IActionResult GetVersion()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+
+            return Ok(new
+            {
+                status = "ok",
+                data = new { version = version?.ToString() ?? "неизвестно" }
+            });
+        }
+    }
+}

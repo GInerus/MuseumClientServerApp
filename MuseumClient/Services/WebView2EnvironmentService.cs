fecefc8efe60b836520367ac05939a4d2e3f939b
Герман Исаев <germanisaev422@gmail.com>
2026-08-20 18:05:47 +0300

    Исправлено обращение к System.Environment.GetFolderPath Устранён конфликт имени Environment с Lazy<Task<CoreWebView2Environment>> Исправлена ошибка компиляции SpecialFolder и GetFolderPath

--- MuseumClient/Services/WebView2EnvironmentService.cs
+++ MuseumClient/Services/WebView2EnvironmentService.cs
@@ -8,15 +8,24 @@ namespace MuseumClient.Services
     public static class WebView2EnvironmentService
     {
         private static readonly string UserDataFolder = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            System.Environment.GetFolderPath(
+                System.Environment.SpecialFolder.LocalApplicationData),
             "MuseumSpace",
             "WebView2");
 
+        private static readonly Lazy<Task<CoreWebView2Environment>> Environment =
+            new Lazy<Task<CoreWebView2Environment>>(CreateEnvironmentAsync);
+
         public static Task<CoreWebView2Environment> CreateAsync()
+        {
+            return Environment.Value;
+        }
+
+        private static async Task<CoreWebView2Environment> CreateEnvironmentAsync()
         {
             Directory.CreateDirectory(UserDataFolder);
 
-            return CoreWebView2Environment.CreateAsync(
+            return await CoreWebView2Environment.CreateAsync(
                 null,
                 UserDataFolder);
         }
